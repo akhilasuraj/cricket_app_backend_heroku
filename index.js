ae590f03f156902118de585b2db83341e8ec579a
jayara <jayarathambavita@gmail.com>
2020-03-29 01:29:43 +0530

    upload test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(
     bodyParser.urlencoded({ extended: false })
 )
-app.use(express.static('uploads'));
+app.use('/uploads',express.static('uploads'));
 
 //connect to db
 dotenv.config();
